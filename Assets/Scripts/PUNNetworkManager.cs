0bfa3fd0dfadea0363328e814e0596462f88c40c
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-12-10 12:17:08 +1300

    Setup CoreSystem Scene with network and voice manager

---
+++ Assets/Scripts/PUNNetworkManager.cs
@@ -0,0 +1,44 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PUNNetworkManager : MonoBehaviourPunCallbacks {
+    private string gameVersion = "1.0";
+    public GameObject avatar;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        PhotonNetwork.Instantiate(avatar.name, new Vector3(0f, 1.5f, 0f), Quaternion.identity, 0);
+    }
+}
