ec8b5c7d3fcae7cf828e667a0fed922e1aeebf1d
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-12-10 13:13:10 +1300

    Add ViewLoader prefab and script

---
+++ Assets/Scripts/ViewLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ViewLoader : MonoBehaviour {
+    private string coreSystemScene = "CoreSystem";
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene(coreSystemScene, LoadSceneMode.Additive);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
