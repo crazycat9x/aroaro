5c3cfddef27e235a7f646da9f759a0f1652dd1e1
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-12-14 11:31:24 +1300

    Controller v4

--- Assets/Scripts/ControllersInputManager.cs
+++ Assets/Scripts/ControllersInputManager.cs
@@ -19,9 +19,78 @@
         public VRTK_Pointer teleportPointer;
 
         /// <summary>
+        /// Defines the dashTime
+        /// </summary>
+        public float dashTime;
+
+        /// <summary>
         /// Defines the boundary
         /// </summary>
         private Transform boundary;
+
+        /// <summary>
+        /// The SelectionButtonPressedHandler
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="args">The args<see cref="ControllerInteractionEventArgs"/></param>
+        private void SelectionButtonPressedHandler(object sender, ControllerInteractionEventArgs args)
+        {
+            // Wait for SDKSetup to load
+            if (boundary == null) return;
+
+            // Touchpad right pressed
+            if (args.touchpadAxis.x > 0.5)
+            {
+                boundary.rotation *= Quaternion.Euler(0, 30, 0);
+            }
+            // Touchpad left pressed
+            else if (args.touchpadAxis.x < -0.5)
+            {
+                boundary.rotation *= Quaternion.Euler(0, -30, 0);
+            }
+            // Touchpad top pressed
+            else if (args.touchpadAxis.y >= 0)
+            {
+                // Must be called twice to be able to turn off again (weird bug)
+                teleportPointer.Toggle(true);
+                teleportPointer.Toggle(true);
+            }
+            // Touchpad bottom pressed
+            else
+            {
+                // TODO: Implement backward dash
+            }
+        }
+
+        /// <summary>
+        /// The SelectionButtonReleasedHandler
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="args">The args<see cref="ControllerInteractionEventArgs"/></param>
+        private void SelectionButtonReleasedHandler(object sender, ControllerInteractionEventArgs args)
+        {
+            if (!teleportPointer.IsStateValid()) teleportPointer.Toggle(false);
+        }
+
+        /// <summary>
+        /// The DestinationMarkerSetHandler
+        /// </summary>
+        /// <param name="__">The __<see cref="object"/></param>
+        /// <param name="___">The ___<see cref="DestinationMarkerEventArgs"/></param>
+        private void DestinationMarkerSetHandler(object __, DestinationMarkerEventArgs ___)
+        {
+            teleportPointer.Toggle(false);
+        }
+
+        /// <summary>
+        /// The TearDownEventHandlers
+        /// </summary>
+        private void TearDownEventHandlers()
+        {
+            teleportPointer.SelectionButtonPressed -= SelectionButtonPressedHandler;
+            teleportPointer.SelectionButtonReleased -= SelectionButtonReleasedHandler;
+            teleportPointer.DestinationMarkerSet -= DestinationMarkerSetHandler;
+        }
 
         /// <summary>
         /// The Awake
@@ -37,6 +106,17 @@
         private void OnEnable()
         {
             boundary = VRTK_DeviceFinder.PlayAreaTransform();
+            teleportPointer.SelectionButtonPressed += SelectionButtonPressedHandler;
+            teleportPointer.SelectionButtonReleased += SelectionButtonReleasedHandler;
+            teleportPointer.DestinationMarkerSet += DestinationMarkerSetHandler;
+        }
+
+        /// <summary>
+        /// The OnDisable
+        /// </summary>
+        private void OnDisable()
+        {
+            TearDownEventHandlers();
         }
 
         /// <summary>
@@ -45,46 +125,7 @@
         private void OnDestroy()
         {
             VRTK_SDKManager.instance.RemoveBehaviourToToggleOnLoadedSetupChange(this);
-        }
-
-        /// <summary>
-        /// The Start
-        /// </summary>
-        internal void Start()
-        {
-            teleportPointer.SelectionButtonPressed += (object _, ControllerInteractionEventArgs args) =>
-            {
-                // Wait for SDKSetup to load
-                if (boundary == null) return;
-
-                // Touchpad right pressed
-                if (args.touchpadAxis.x > 0.5)
-                {
-                    boundary.rotation *= Quaternion.Euler(0, 30, 0);
-                }
-                // Touchpad left pressed
-                else if (args.touchpadAxis.x < -0.5)
-                {
-                    boundary.rotation *= Quaternion.Euler(0, -30, 0);
-                }
-                // Touchpad top pressed
-                else if (args.touchpadAxis.y >= 0)
-                {
-                    // Must be called twice to be able to turn off again (weird bug)
-                    teleportPointer.Toggle(true);
-                    teleportPointer.Toggle(true);
-                }
-            };
-
-            teleportPointer.SelectionButtonReleased += (object _, ControllerInteractionEventArgs args) =>
-            {
-                if (!teleportPointer.IsStateValid()) teleportPointer.Toggle(false);
-            };
-
-            teleportPointer.DestinationMarkerSet += (object __, DestinationMarkerEventArgs ___) =>
-            {
-                teleportPointer.Toggle(false);
-            };
+            TearDownEventHandlers();
         }
     }
 }
