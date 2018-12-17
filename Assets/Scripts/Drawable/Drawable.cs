ba192b6becdb78bb7ab23be30e69463b47603fed
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-12-17 14:46:56 +1300

    Sync users drawing

--- Assets/Scripts/Drawable/Drawable.cs
+++ Assets/Scripts/Drawable/Drawable.cs
@@ -1,5 +1,6 @@
 ﻿namespace Aroaro
 {
+    using Photon.Pun;
     using System.Collections.Generic;
     using System.Linq;
     using UnityEngine;
@@ -7,6 +8,7 @@
     /// <summary>
     /// Defines the <see cref="Drawable" />
     /// </summary>
+    [RequireComponent(typeof(PhotonView))]
     public class Drawable : MonoBehaviour
     {
         /// <summary>
@@ -36,13 +38,30 @@
         /// <param name="position">The position<see cref="Vector2"/></param>
         /// <param name="penSize">The penSize<see cref="int"/></param>
         /// <param name="penColor">The penColor<see cref="Color"/></param>
-        public void Draw(int penId, Vector2 position, int penSize, Color penColor)
+        public void Draw(int penId, Vector2 position, int penSize, Color32 penColor)
         {
+            gameObject.GetComponent<PhotonView>().RPC("DrawAndBroadCast", RpcTarget.AllBufferedViaServer, penId, position, penSize, penColor.r, penColor.g, penColor.b, penColor.a);
+        }
+
+        /// <summary>
+        /// The DrawAndBroadCast
+        /// </summary>
+        /// <param name="penId">The penId<see cref="int"/></param>
+        /// <param name="position">The position<see cref="Vector2"/></param>
+        /// <param name="penSize">The penSize<see cref="int"/></param>
+        /// <param name="r">The r<see cref="byte"/></param>
+        /// <param name="g">The g<see cref="byte"/></param>
+        /// <param name="b">The b<see cref="byte"/></param>
+        /// <param name="a">The a<see cref="byte"/></param>
+        [PunRPC]
+        private void DrawAndBroadCast(int penId, Vector2 position, int penSize, byte r, byte g, byte b, byte a)
+        {
+            Color32 penColor = new Color32(r, g, b, a);
             int x = Mathf.RoundToInt(position.x * texture.height - (penSize / 2));
             int y = Mathf.RoundToInt(position.y * texture.width - (penSize / 2));
-            Color[] colors = Enumerable.Repeat<Color>(penColor, penSize * penSize).ToArray<Color>();
+            Color32[] colors = Enumerable.Repeat<Color32>(penColor, penSize * penSize).ToArray<Color32>();
 
-            texture.SetPixels(x, y, penSize, penSize, colors);
+            texture.SetPixels32(x, y, penSize, penSize, colors);
 
             Vector2 previousPosition;
             if (previousPositions.TryGetValue(penId, out previousPosition))
@@ -51,10 +70,20 @@
                 {
                     int lerpX = Mathf.RoundToInt(Mathf.Lerp(previousPosition.x, (float)x, t));
                     int lerpY = Mathf.RoundToInt(Mathf.Lerp(previousPosition.y, (float)y, t));
-                    texture.SetPixels(lerpX, lerpY, penSize, penSize, colors);
+                    texture.SetPixels32(lerpX, lerpY, penSize, penSize, colors);
                 }
             }
             previousPositions[penId] = new Vector2(x, y);
+        }
+
+        /// <summary>
+        /// The EndStroke
+        /// </summary>
+        /// <param name="penId">The penId<see cref="int"/></param>
+        [PunRPC]
+        private void EndStroke(int penId)
+        {
+            previousPositions.Remove(penId);
         }
 
         /// <summary>
@@ -63,7 +92,7 @@
         /// <param name="collision">The collision<see cref="Collision"/></param>
         private void OnCollisionExit(Collision collision)
         {
-            if (collision.gameObject.GetComponent<Pen>() != null) previousPositions.Remove(collision.gameObject.GetInstanceID());
+            if (collision.gameObject.GetComponent<Pen>() != null) gameObject.GetComponent<PhotonView>().RPC("EndStroke", RpcTarget.AllBufferedViaServer, collision.gameObject.GetInstanceID());
         }
 
         /// <summary>
@@ -83,7 +112,7 @@
         /// </summary>
         private void Update()
         {
-            if (previousPositions.Count() > 0) texture.Apply();
+            texture.Apply();
         }
     }
 }
