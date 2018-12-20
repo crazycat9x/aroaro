1c6237898b1d2b9e2212e70c71b3ae149f78ba24
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-12-20 13:39:05 +1300

    Attach control panel to user left hand

---
+++ Assets/Scripts/ControlPanelManager.cs
@@ -0,0 +1,46 @@
+﻿namespace Aroaro
+{
+    using UnityEngine;
+    using VRTK;
+
+    /// <summary>
+    /// Defines the <see cref="ControlPanelManager" />
+    /// </summary>
+    public class ControlPanelManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Defines the snapJoint
+        /// </summary>
+        public VRTK_TransformFollow snapJoint;
+
+        /// <summary>
+        /// Defines the leftController
+        /// </summary>
+        private GameObject leftController;
+
+        /// <summary>
+        /// The Awake
+        /// </summary>
+        internal void Awake()
+        {
+            VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+        }
+
+        /// <summary>
+        /// The OnEnable
+        /// </summary>
+        internal void OnEnable()
+        {
+            leftController = VRTK_DeviceFinder.GetControllerLeftHand();
+            snapJoint.gameObjectToFollow = leftController;
+        }
+
+        /// <summary>
+        /// The OnDestroy
+        /// </summary>
+        internal void OnDestroy()
+        {
+            VRTK_SDKManager.instance.RemoveBehaviourToToggleOnLoadedSetupChange(this);
+        }
+    }
+}
