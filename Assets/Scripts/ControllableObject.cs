7ab5a0b110eac82dc1d1d4893b1d73823763a07b
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2019-01-10 16:45:27 +1300

    Spawn menu above instantiated object, add delete button to menu

---
+++ Assets/Scripts/ControllableObject.cs
@@ -0,0 +1,47 @@
+﻿namespace Aroaro
+{
+    using Photon.Pun;
+    using UnityEngine;
+
+    /// <summary>
+    /// Defines the <see cref="ControllableObject" />
+    /// </summary>
+    public class ControllableObject : MonoBehaviourPun
+    {
+        /// <summary>
+        /// The DestroyObject
+        /// </summary>
+        public void DestroyObject()
+        {
+            if (photonView != null)
+            {
+                photonView.RPC("DestroyNetworkedObject", RpcTarget.All);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        /// <summary>
+        /// The DestroyNetworkedObject
+        /// </summary>
+        [PunRPC]
+        private void DestroyNetworkedObject()
+        {
+            if (photonView.IsMine) PhotonNetwork.Destroy(gameObject);
+        }
+
+        /// <summary>
+        /// The Start
+        /// </summary>
+        internal void Start()
+        {
+            GameObject objectMenu = Resources.Load<GameObject>("ObjectMenu");
+            ObjectMenu objectMenuScript = objectMenu.GetComponent<ObjectMenu>();
+            objectMenuScript.targetGameObject = transform.gameObject;
+
+            Instantiate(objectMenu, transform.position + new Vector3(0, transform.localScale.z, 0), transform.rotation, transform);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectMenu.cs
@@ -0,0 +1,48 @@
+﻿namespace Aroaro
+{
+    using UnityEngine;
+    using VRTK;
+
+    /// <summary>
+    /// Defines the <see cref="ObjectMenu" />
+    /// </summary>
+    public class ObjectMenu : MonoBehaviour
+    {
+        /// <summary>
+        /// Defines the targetGameObject
+        /// </summary>
+        public GameObject targetGameObject;
+
+        /// <summary>
+        /// Defines the controllableObject
+        /// </summary>
+        [HideInInspector]
+        public ControllableObject controllableObject;
+
+        /// <summary>
+        /// The DestroyObject
+        /// </summary>
+        public void DestroyObject()
+        {
+            controllableObject.DestroyObject();
+        }
+
+        /// <summary>
+        /// The Start
+        /// </summary>
+        internal void Awake()
+        {
+            controllableObject = targetGameObject.GetComponent<ControllableObject>();
+            if (controllableObject == null)
+                controllableObject = targetGameObject.AddComponent<ControllableObject>();
+        }
+
+        /// <summary>
+        /// The Update
+        /// </summary>
+        internal void Update()
+        {
+            transform.rotation = Quaternion.LookRotation(transform.position - VRTK_DeviceFinder.HeadsetTransform().position);
+        }
+    }
+}
